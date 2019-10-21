511d46dbfb8028273c71a9090d615a43812cc7ab
Wilbelison Junior <meustudio@gmail.com>
2019-10-21 08:36:46 -0300

    partial home
    partial home for desktop with video background hero, nav and footer

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(
+	function () {
+
+        var video = document.getElementById('video');
+        video.playbackRate = 0.75;
+
+	}
+);
+
+/* removedor de acentos */
+
+function removeAcentos (text) {       
+    text = text.toLowerCase();                                                         
+    text = text.replace(new RegExp('[ÁÀÂÃ]','gi'), 'a');
+    text = text.replace(new RegExp('[ÉÈÊ]','gi'), 'e');
+    text = text.replace(new RegExp('[ÍÌÎ]','gi'), 'i');
+    text = text.replace(new RegExp('[ÓÒÔÕ]','gi'), 'o');
+    text = text.replace(new RegExp('[ÚÙÛ]','gi'), 'u');
+    text = text.replace(new RegExp('[Ç]','gi'), 'c');
+    return text;                 
+}
