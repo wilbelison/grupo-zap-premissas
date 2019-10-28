1197d6caf7b12af3c9cb2e45b74c1e8cbca53685
Wilbelison Junior <meustudio@gmail.com>
2019-10-28 06:48:40 -0300

    form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,63 @@
 $(document).ready(
 	function () {
 
-        
+
 
 	}
 );
+
+/* formspree.io */
+
+window.addEventListener("DOMContentLoaded", function() {
+
+    var form = document.getElementById("fale-form");
+    var button = document.getElementById("fale-submit");
+    var status = document.getElementById("fale-status");
+    var reset = document.getElementById("fale-reset");
+    
+    function success() {
+        form.reset();
+        button.style = "display: none";
+        status.innerHTML = "Envidado ;)";
+    }
+
+    function limpar() {
+        form.reset();
+        button.style = "display: block";
+        status.innerHTML = "";
+    }
+
+    function error() {
+        status.innerHTML = "Oops! Ocorreu um erro :(";
+    }
+
+    form.addEventListener("submit", function(ev) {
+        ev.preventDefault();
+        var data = new FormData(form);
+        ajax(form.method, form.action, data, success, error);
+    });
+
+    reset.addEventListener("click", function(ev) {
+        ev.preventDefault();
+        limpar();
+    });
+
+});
+
+function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState !== XMLHttpRequest.DONE) return;
+        if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+        } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+        }
+    };
+    xhr.send(data);
+}
 
 /* video mais lento em 25% */
 
